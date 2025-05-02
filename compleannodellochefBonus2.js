2fd2fbf183d03a50be33cae7ffff35c1bc1197ba
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-05-02 21:47:34 +0200

    bonus2

---
+++ compleannodellochefBonus2.js
@@ -0,0 +1,44 @@
+
+async function getChefBirthday(id) {
+    let receipes
+    try {
+        const promiseReceipes = await fetch(`https://dummyjson.com/recipes/${id}`)
+        receipes = await promiseReceipes.json()
+    } catch (error) {
+        console.log(error)
+       throw Error ("errore fetch receipe", error) 
+    }
+
+    if (receipes.message){
+
+        throw Error ("errore  receipe non trovata") 
+    }
+
+    let chef
+    try {
+        const promiseChef = await fetch(`https://dummyjson.com/users/${receipes.userId}`)
+        chef = await promiseChef.json()
+        
+    } catch (error) {
+        console.log(error)
+        throw Error ("errore fetch chef", error) 
+    }
+    if (chef.message){
+        throw Error ("errore fetch receipe non trovata") 
+    }
+    const formattedDate = dayjs(chef.birthDate).format('DD/MM/YYYY') 
+    
+    return  formattedDate
+    
+
+}
+
+(async () => {
+    try {
+        const result = await getChefBirthday(1)
+        console.log("La data di nascita dello chef è : ", result)
+    } catch (error) {
+        console.log(error)
+    }
+
+})()
