18354df544cbf81a822f8e7ded023c8ff588df7c
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-05-02 21:10:16 +0200

    Obbiettivo 1

--- compleannodellochef.js
+++ compleannodellochef.js
@@ -1,3 +1,20 @@
-async function getChefBirthday(id){
-    const receipes = await fetch("https://dummyjson.com/recipes/{id}")
+
+async function getChefBirthday(id) {
+
+    const promiseReceipes = await fetch(`https://dummyjson.com/recipes/${id}`)
+    const receipes = await promiseReceipes.json()
+    const promiseUsers = await fetch(`https://dummyjson.com/users/${receipes.id}`)
+    const user = await promiseUsers.json()
+    return users.birthDate
+
 }
+
+(async ()=>{
+   try {
+    const result = await getChefBirthday(1)
+    console.log("La data di nascita dello chef è : ",result)
+   } catch (error) {
+    console.log(error)
+   }
+   
+})()
