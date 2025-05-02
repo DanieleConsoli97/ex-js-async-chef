0d5b8996ac5d0bd548d704e1701c2bddc0380309
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-05-02 21:38:18 +0200

    bonus 1

--- compleannodellochef.js
+++ compleannodellochef.js
@@ -1,20 +1,43 @@
 
 async function getChefBirthday(id) {
+    let receipes
+    try {
+        const promiseReceipes = await fetch(`https://dummyjson.com/recipes/${id}`)
+        receipes = await promiseReceipes.json()
+    } catch (error) {
+        console.log(error)
+       throw Error ("errore fetch receipe", error) 
+    }
 
-    const promiseReceipes = await fetch(`https://dummyjson.com/recipes/${id}`)
-    const receipes = await promiseReceipes.json()
-    const promiseUsers = await fetch(`https://dummyjson.com/users/${receipes.id}`)
-    const user = await promiseUsers.json()
-    return users.birthDate
+    if (receipes.message){
+
+        throw Error ("errore  receipe non trovata") 
+    }
+
+    let chef
+    try {
+        const promiseChef = await fetch(`https://dummyjson.com/users/${receipes.userId}`)
+        chef = await promiseChef.json()
+        
+    } catch (error) {
+        console.log(error)
+        throw Error ("errore fetch chef", error) 
+    }
+    if (!chef){
+        throw Error ("errore fetch receipe non trovata") 
+    }
+
+return chef.birthDate
+    
 
 }
 
-(async ()=>{
-   try {
-    const result = await getChefBirthday(1)
-    console.log("La data di nascita dello chef è : ",result)
-   } catch (error) {
-    console.log(error)
-   }
-   
+(async () => {
+    try {
+        const result = await getChefBirthday(1)
+        console.log("La data di nascita dello chef è : ", result)
+    } catch (error) {
+        console.log(error)
+    }
+
 })()
